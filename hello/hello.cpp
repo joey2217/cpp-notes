072fa5c6f4c0bbb24a016e119387fdd44f25b2d9
Joey <786678257@qq.com>
2024-07-10 17:58:33 +0800

    feat(vscode): update config files and add new features for C++ development

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,9 +1,14 @@
 #include "iostream"
+#include "stdint.h"
+
 using namespace std;
 
 int main()
 {
+    uint16_t a = 0;
     cout << "Hello World!" << endl;
+    string str2 = "google";
+    cout << str2 << endl;
     system("pause");
     return 0;
 }
